96e3db12402825ba357b764652d9a5cc294ac819
AlanKostrick <alan@wecancodeit.org>
2019-09-11 23:20:12 -0500

    extracted db

--- blog-post-api/app.js
+++ blog-post-api/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-const db = mongoose.connection;
-db.dropDatabase();
+const app = express();
 const port = 3000;
 
-const app = express();
+require("./src/models/db");
 
 const cors = require('cors');
 app.use(cors());
@@ -14,16 +11,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('Successful connection!'));
-
-
-const Post = require('./src/models/Post');
-
 const postsRouter = require("./src/routes/posts");
 app.use("/posts", postsRouter);
-
-
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- blog-post-api/src/routes/posts.js
+++ blog-post-api/src/routes/posts.js
@@ -10,7 +10,6 @@ router.get('/', (req, res) => {
     })
 })
 
-
 router.get('/:postId', (req, res) => {
     Post.findById(req.params.postId, (error, data) => {
         if (error) return res.sendStatus(500).json(error)
@@ -18,12 +17,10 @@ router.get('/:postId', (req, res) => {
     })
 })
 
-
 router.post('/', (req, res) => {
     const newPost = new Post(req.body)
     newPost.save((err, post) => { return err ? res.sendStatus(500).json(err) : res.json(post) })
 })
-
 
 router.put('/:postId', (req, res) => {
     Post.findByIdAndUpdate(req.params.postId, { $set: { title: req.body.title, body: req.body.body } }, (error, data) => {
